7477630c44d944a9632d9714aded85d3376bec6d
Tom Liebmann <kontakt@liebmann-online.de>
2023-12-08 06:44:54 +0100

    Add solution for day 8 part 2

---
+++ solutions/08_2/main.cpp
@@ -0,0 +1,160 @@
+#include <cmath>
+#include <cstdlib>
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <regex>
+#include <unordered_set>
+
+
+#include <stream_utils.hpp>
+#include <string_utils.hpp>
+
+
+namespace
+{
+    struct Node
+    {
+        std::string name;
+        std::string leftName;
+        std::string rightName;
+
+        static Node parse( std::string const& line );
+    };
+
+    std::unordered_map< std::string, Node > parseNodes( std::istream& stream );
+
+    long computePathLen( std::string const& instructions,
+                         std::unordered_map< std::string, Node > const& nodes,
+                         std::string const& node );
+
+    std::unordered_map< long, long > computePrimeFactors( long n );
+}
+
+
+int main( int argc, char** argv )
+{
+    if( argc < 2 )
+    {
+        std::cerr << "Missing parameter: <input file>\n";
+        return EXIT_FAILURE;
+    }
+
+    auto const fileName = std::string{ argv[ 1 ] };
+    auto fileStream = std::ifstream{ fileName };
+
+    auto instructions = std::string{};
+    std::getline( fileStream, instructions );
+
+    auto const nodes = parseNodes( fileStream );
+
+    auto sharedFactors = std::unordered_map< long, long >{};
+
+    for( auto const& node : nodes )
+    {
+        auto const& name = node.first;
+        if( name[ name.size() - 1 ] != 'A' )
+        {
+            continue;
+        }
+        auto const pathLen = computePathLen( instructions, nodes, name );
+        auto const factors = computePrimeFactors( pathLen );
+
+        for( auto const& f : factors )
+        {
+            sharedFactors[ f.first ] = std::max( sharedFactors[ f.first ], f.second );
+        }
+    }
+
+    auto result = 1L;
+    for( auto const& f : sharedFactors )
+    {
+        result *= std::pow( f.first, f.second );
+    }
+
+    std::cout << "Node count: " << result << '\n';
+
+    return EXIT_SUCCESS;
+}
+
+namespace
+{
+    long computePathLen( std::string const& instructions,
+                         std::unordered_map< std::string, Node > const& nodes,
+                         std::string const& node )
+    {
+        auto currentNode = node;
+        auto nodeCount = 0L;
+
+        while( true )
+        {
+            for( auto const c : instructions )
+            {
+                if( currentNode[ currentNode.size() - 1 ] == 'Z' )
+                {
+                    return nodeCount;
+                }
+                if( c == 'L' )
+                {
+                    currentNode = nodes.at( currentNode ).leftName;
+                }
+                else
+                {
+                    currentNode = nodes.at( currentNode ).rightName;
+                }
+
+                ++nodeCount;
+            }
+        }
+    }
+
+    std::unordered_map< long, long > computePrimeFactors( long n )
+    {
+        auto factors = std::unordered_map< long, long >{};
+        while( n % 2 == 0 )
+        {
+            ++factors[ 2 ];
+            n = n / 2;
+        }
+
+        for( int i = 3; i <= sqrt( n ); i = i + 2 )
+        {
+            while( n % i == 0 )
+            {
+                ++factors[ i ];
+                n = n / i;
+            }
+        }
+        if( n > 2 )
+            ++factors[ n ];
+
+        return factors;
+    }
+
+    std::unordered_map< std::string, Node > parseNodes( std::istream& stream )
+    {
+        auto nodes = std::unordered_map< std::string, Node >{};
+        iterateLines( stream,
+                      [ & ]( auto const& line )
+                      {
+                          auto node = Node::parse( line );
+                          nodes[ node.name ] = node;
+                      } );
+
+        return nodes;
+    }
+
+    Node Node::parse( std::string const& line )
+    {
+        auto const pattern = std::regex{ R"((\w{3})\s*=\s*\((\w{3})\s*,\s*(\w{3})\))" };
+        auto match = std::smatch{};
+        if( !std::regex_match( line, match, pattern ) )
+        {
+            throw std::runtime_error( fmt::format( "Line did not match: {}", line ) );
+        }
+
+        return { match[ 1 ], match[ 2 ], match[ 3 ] };
+    }
+}
