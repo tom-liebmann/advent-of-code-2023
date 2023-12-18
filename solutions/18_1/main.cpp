51f195e3fe701dbb69923cbcbacf86326fbe5fc5
Tom Liebmann <kontakt@liebmann-online.de>
2023-12-18 07:41:59 +0100

    Improve solutuon for day 18 part 1

--- solutions/18_1/main.cpp
+++ solutions/18_1/main.cpp
@@ -47,8 +47,7 @@ namespace
         int y;
     };
 
-    std::generator< std::tuple< Pos, Dir, Dir > >
-        traverse( std::vector< Instruction > const& instructions, Pos start );
+    std::generator< Pos > traverse( std::vector< Instruction > const& instructions, Pos start );
 }
 
 
@@ -68,63 +67,24 @@ long Application::computeResult( std::istream& inputStream )
         instructions.push_back( Instruction::parse( line ) );
     }
 
-    auto max = Pos{ 0, 0 };
-    auto min = Pos{ 0, 0 };
-    for( auto const& [ pos, inDir, outDir ] : traverse( instructions, { 0, 0 } ) )
+    auto prevPos = Pos{ 0, 0 };
+
+    auto area = 0L;
+    auto tiles = 0L;
+    for( auto const& pos : traverse( instructions, Pos{ 0, 0 } ) )
     {
-        min.x = std::min( min.x, pos.x );
-        min.y = std::min( min.y, pos.y );
-        max.x = std::max( max.x, pos.x );
-        max.y = std::max( max.y, pos.y );
+        area += ( prevPos.y + pos.y ) * ( prevPos.x - pos.x );
+        tiles += std::abs( ( pos.x - prevPos.x ) + ( pos.y - prevPos.y ) );
+        prevPos = pos;
     }
 
-    auto const DIRS = std::map< std::pair< Dir, Dir >, char >{
-        { { Dir::LEFT, Dir::LEFT }, '-' }, { { Dir::LEFT, Dir::DOWN }, 'F' },
-        { { Dir::LEFT, Dir::UP }, 'L' },   { { Dir::RIGHT, Dir::RIGHT }, '-' },
-        { { Dir::RIGHT, Dir::UP }, '/' },  { { Dir::RIGHT, Dir::DOWN }, '7' },
-        { { Dir::UP, Dir::UP }, '|' },     { { Dir::UP, Dir::LEFT }, '7' },
-        { { Dir::UP, Dir::RIGHT }, 'F' },  { { Dir::DOWN, Dir::DOWN }, '|' },
-        { { Dir::DOWN, Dir::LEFT }, '/' }, { { Dir::DOWN, Dir::RIGHT }, 'L' },
-    };
+    auto const innerTiles = std::abs( area / 2 ) - ( tiles - 4 ) / 2 - 1;
+    auto const total = innerTiles + tiles;
 
-    auto lagoon = Grid< char >{ max.x - min.x + 1, max.y - min.y + 1, '.' };
-    for( auto const& [ pos, inDir, outDir ] : traverse( instructions, { 0, 0 } ) )
-    {
-        // fmt::print( "{} {}\n", static_cast< int >( inDir ), static_cast< int >( outDir ) );
-        lagoon( pos.x - min.x, pos.y - min.y ) = DIRS.at( std::make_pair( inDir, outDir ) );
-    }
+    fmt::print( "Area: {} {}\n", area, tiles );
+    fmt::print( "Result: {} {}\n", innerTiles, total );
 
-    auto sum = 0L;
-    for( int y = 0; y < lagoon.getHeight(); ++y )
-    {
-        auto inside = false;
-        for( int x = 0; x < lagoon.getWidth(); ++x )
-        {
-            if( lagoon( x, y ) == '.' )
-            {
-                if( inside )
-                {
-                    lagoon( x, y ) = '#';
-                    ++sum;
-                }
-            }
-            else
-            {
-                switch( lagoon( x, y ) )
-                {
-                    case '7':
-                    case '|':
-                    case 'F':
-                        inside = !inside;
-                }
-                ++sum;
-            }
-            fmt::print( "{}", lagoon( x, y ) );
-        }
-        fmt::print( "\n" );
-    }
-
-    return sum;
+    return total;
 }
 
 namespace
@@ -153,31 +113,25 @@ namespace
         return Instruction{ dir, std::stoi( match[ 2 ] ), match[ 3 ] };
     }
 
-    std::generator< std::tuple< Pos, Dir, Dir > >
-        traverse( std::vector< Instruction > const& instructions, Pos start )
+    std::generator< Pos > traverse( std::vector< Instruction > const& instructions, Pos start )
     {
-        auto prevDir = instructions[ instructions.size() - 1 ].dir;
         for( auto const& instr : instructions )
         {
-            for( int i = 0; i < instr.len; ++i )
+            switch( instr.dir )
             {
-                co_yield std::make_tuple( start, prevDir, instr.dir );
-                switch( instr.dir )
-                {
-                    case Dir::RIGHT:
-                        ++start.x;
-                        break;
-                    case Dir::UP:
-                        --start.y;
-                        break;
-                    case Dir::LEFT:
-                        --start.x;
-                        break;
-                    case Dir::DOWN:
-                        ++start.y;
-                }
-                prevDir = instr.dir;
+                case Dir::RIGHT:
+                    start.x += instr.len;
+                    break;
+                case Dir::UP:
+                    start.y -= instr.len;
+                    break;
+                case Dir::LEFT:
+                    start.x -= instr.len;
+                    break;
+                case Dir::DOWN:
+                    start.y += instr.len;
             }
+            co_yield start;
         }
     }
 }
